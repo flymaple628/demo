dcf562997af95afaa5945d00740ed39eeda84a9c
Aaron Chen <flymaple628@gmail.com>
2015-06-10 10:36:01 +0800

    initialize

---
+++ test/controllers/welcome_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class WelcomeControllerTest < ActionController::TestCase
+  def say
+  	<h1>Hello, World!</h1>
+  end
+  # test "the truth" do
+  #   assert true
+  # end
+end
